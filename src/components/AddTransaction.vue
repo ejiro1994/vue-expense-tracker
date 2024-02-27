551673f67a8c14b0d1c3d25495fe54cc581f4d56
ejiro1994 <ejiro1994@gmail.com>
2024-02-27 00:29:52 +0000

    first commit

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,14 @@
+<template>
+  <h3>Add new transaction</h3>
+  <form id="form">
+    <div class="form-control">
+      <label for="text">Text</label>
+      <input type="text" id="text" placeholder="text" />
+    </div>
+    <div class="form-control">
+        <label for="amount">Amount</label>
+        <input type="number" id="amount" placeholder="amount">
+        <button class="btn">Add transaction</button>
+    </div>
+  </form>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,21 @@
+<script setup>
+  const transactions = [
+    { id: 1, text: "Flower", amount: -19.99 },
+    { id: 2, text: "Cake", amount: -10.99 },
+    { id: 3, text: "Camera", amount: -500.0 },
+  ];
+</script>
+
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      Cash <span>{{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+  </ul>
+</template>
